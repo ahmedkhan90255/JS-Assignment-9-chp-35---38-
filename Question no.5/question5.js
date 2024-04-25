706fd3ba6b5bf87f74022bd0b9060fc6ced06ebc
ahmedkhan90255 <ahmed.m.khan123@gmail.com>
2024-04-24 18:37:39 -0700

    5 questions completed

---
+++ Question no.5/question5.js
@@ -0,0 +1,9 @@
+document.write("<h1> Write a function that squares its argument. </h1>")
+
+var inputNum = parseInt(prompt("Enter Any Number"))
+
+function TakingPower(inputNum) {
+    var result = inputNum * inputNum
+    document.write("<h2>Ans: The square of " + inputNum + " is " + result + "</h2>")
+}
+TakingPower(inputNum)
